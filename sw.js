f7f6aeaaf321034b7dab4098c2861fcb8de4d7ad
reeselee7424-stack <reeselee7424@gmail.com>
2025-12-25 14:22:43 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gummy-match-v1";
+const CACHE_NAME = "gummy-match-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -31,3 +31,4 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
+
